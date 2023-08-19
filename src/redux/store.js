49177c12a47b8cf3752a33f2a1bc086014f9611f
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-19 19:22:59 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
-  // FLUSH,
-  // REHYDRATE,
-  // PAUSE,
-  // PERSIST,
-  // PURGE,
-  // REGISTER,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactSlice, filterSlice } from './contacts/slice';
@@ -25,13 +25,12 @@ export const store = configureStore({
     contacts: contactSlice.reducer,
     filter: filterSlice.reducer,
   },
-  // ,
-  // middleware: getDefaultMiddleware =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   // devTools: process.env.NODE_ENV === 'development',
   // якщо розкоментувати не будуть відображатись данні у девтулзах
 });
