ec2261937a70ea0a360637c2f6230112b503399a
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-19 18:48:51 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactSlice, filterSlice } from './contacts/slice';
 import { authReducer } from './auth/slice';
@@ -25,13 +16,6 @@ export const store = configureStore({
     contacts: contactSlice.reducer,
     filter: filterSlice.reducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
