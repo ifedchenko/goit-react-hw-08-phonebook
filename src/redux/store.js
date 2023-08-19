a0839de28c0809994a31206c8d4379e87610f7ad
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-08-19 19:15:34 +0300

    showing devtools actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactSlice, filterSlice } from './contacts/slice';
 import { authReducer } from './auth/slice';
@@ -16,6 +25,15 @@ export const store = configureStore({
     contacts: contactSlice.reducer,
     filter: filterSlice.reducer,
   },
+  // ,
+  // middleware: getDefaultMiddleware =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
+  // devTools: process.env.NODE_ENV === 'development',
+  // якщо розкоментувати не будуть відображатись данні у девтулзах
 });
 
 export const persistor = persistStore(store);
